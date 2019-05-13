db1356c87223afb18f6f30da30aa8880fac1b561
Arion12112 <47907685+Arion12112@users.noreply.github.com>
2019-05-13 23:30:35 +0700

    test items

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public LayerMask movementMask;
+    public Interactable focus;  
 
     Camera cam;
     PlayerMotor motor;
@@ -31,6 +32,7 @@ public class PlayerController : MonoBehaviour
                 //move player to what we hit
                 motor.MoveToPoint(hit.point);
                 //stop focusing any objects
+                RemoveFocus();
             } 
         }
 
@@ -42,8 +44,37 @@ public class PlayerController : MonoBehaviour
             if (Physics.Raycast(ray, out hit, 100))
             {
                 //check if we hit interactable
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
                 //if we did set it as our focus
+                if(interactable != null)
+                {
+                    SetFocus(interactable);   
+                }
             }
         }
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        if(newFocus != focus)
+        {
+            if(focus!=null)
+                focus.OnDefocused();
+
+            focus = newFocus;
+            motor.FollowTarget(newFocus);
+        }
+        
+        newFocus.OnFocused(transform);
+        
+    }
+
+    void RemoveFocus()
+    {
+        if(focus!=null)
+            focus.OnDefocused();
+
+        focus = null;
+        motor.StopFollowingTarget();
     }
 }
