af98215a342d4648c421418d10c732859962c27d
hilmifr22 <hilmi.fr98@gmail.com>
2019-05-20 17:32:24 +0700

    Click Sound Script

---
+++ Assets/Game/Scripts/ClickSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Button))]
+public class ClickSound : MonoBehaviour
+{
+    public AudioClip Sound;
+
+    private Button button { get { return GetComponent<Button>(); } }
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = Sound;
+        source.playOnAwake = false;
+
+        button.onClick.AddListener(() => PlaySound());
+    }
+
+    void PlaySound()
+    {
+        source.PlayOneShot(Sound);
+    }
+}
