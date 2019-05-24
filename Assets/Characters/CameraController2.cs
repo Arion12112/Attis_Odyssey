99740df8f419245f781b994b93608ba58021f584
ghifarihaekal7 <38685156+ghifarihaekal7@users.noreply.github.com>
2019-05-24 14:12:08 +0700

    Character Movable Complete Files

---
+++ Assets/Characters/CameraController2.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 1. Follow on player's X/Z plane
+/// 2. Smooth rotation around the player ito 45 degree increments
+/// </summary>
+public class CameraController2 : MonoBehaviour
+{
+    public Transform target2;
+    public Vector3 offsetPos2;
+    public float moveSpeed2 = 5;
+    public float turnSpeed2 = 10;
+    public float smoothSpeed2 = 0.5f;
+
+    Quaternion targetRotation2;
+    Vector3 targetPos2;
+    bool smoothRotating = false;
+
+    void Update()
+    {
+        MoveWithTarget();
+        LookAtTarget();
+
+        if (Input.GetKeyDown(KeyCode.G) && !smoothRotating)
+        {
+            StartCoroutine("RotateAroundTarget", 45);
+        }
+
+        if (Input.GetKeyDown(KeyCode.H) && !smoothRotating)
+        {
+            StartCoroutine("RotateAroundTarget", -45);
+        }
+    }
+    /// <summary>
+    /// Move the camera to the player position + current camera offset
+    /// Offset modified by RotateAroundTarget coroutine
+    /// </summary>
+    void MoveWithTarget()
+    {
+        targetPos2 = target2.position + offsetPos2;
+        transform.position = Vector3.Lerp(transform.position, targetPos2, moveSpeed2 * Time.deltaTime);
+    }
+    /// <summary>
+    /// Use the Look vector (target - current) to aim the camera towards the player
+    /// </summary>
+    void LookAtTarget()
+    {
+        targetRotation2 = Quaternion.LookRotation(target2.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation2, turnSpeed2 * Time.deltaTime);
+    }
+    /// <summary>
+    /// This coroutine can only have an instance running at a time
+    /// Determined by "smoothRotating"
+    /// </summary>
+    IEnumerator RotateAroundTarget(float angle)
+    {
+        Vector3 vel = Vector3.zero;
+        Vector3 targetOffsetPos = Quaternion.Euler(0, angle, 0) * offsetPos2;
+        float dist = Vector3.Distance(offsetPos2, targetOffsetPos);
+        smoothRotating = true;
+
+        while (dist > 0.02f)
+        {
+            offsetPos2 = Vector3.SmoothDamp(offsetPos2, targetOffsetPos, ref vel, smoothSpeed2);
+            dist = Vector3.Distance(offsetPos2, targetOffsetPos);
+            yield return null;
+        }
+
+        smoothRotating = false;
+        offsetPos2 = targetOffsetPos;
+    }
+}
---
+++ Assets/Characters/MarioController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 1. 8-directional movement
+/// 2. Stop and face current direction when input is absent
+/// </summary>
+public class MarioController : MonoBehaviour
+{
+    public float velocity = 5;
+    public float turnSpeed = 10;
+
+    Vector2 input;
+    float angle;
+
+    Quaternion targetRotation;
+    Transform cam;
+
+    Animator anim;
+
+    void Start()
+    {
+        cam = Camera.main.transform;
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        GetInput();
+
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) return;
+
+        CalculateDirection();
+        Rotate();
+        Move();
+    }
+    /// <summary>
+    /// Input based on Horizontal(a,d,<,>)  and Vertical(w,s,^,v) keys
+    /// </summary>
+    void GetInput()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+        anim.SetFloat("BlendX", input.x);
+        anim.SetFloat("BlendY", input.y);
+    }
+    /// <summary>
+    /// Direction relation to the camera's rotation
+    /// </summary>
+    void CalculateDirection()
+    {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+
+    }
+    /// <summary>
+    /// Rotate toward the calculated angle
+    /// </summary>
+    void Rotate()
+    {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+    /// <summary>
+    /// The player only moves along its forward axis
+    /// </summary>
+    void Move()
+    {
+        transform.position += transform.forward * velocity * Time.deltaTime;
+    }
+}
+
